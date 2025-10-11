db8684bfdbad7fb353beb9fe4bbfd2f5909fd30d
ASHOK S <ashoks23it@psnacet.edu.in>
2025-10-11 18:10:00 +0530

    Add files via upload

---
+++ Counter Using FXProperties/src/counter/using/fxproperties/CounterUsingFXProperties.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package counter.using.fxproperties;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author D E L L
+ */
+public class CounterUsingFXProperties extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root=FXMLLoader.load(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Counter");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
